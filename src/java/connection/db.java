e21feb57c55e6f2a74d1b653273010b188fc9581
PCUSER <PCUSER@MPK03-35>
2017-11-16 12:08:19 +0800

    new

---
+++ src/java/connection/db.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+/**
+ *
+ * @author PCUSER
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+  
+public class db {
+  
+ public static Connection getMySQLConnection()
+         throws ClassNotFoundException, SQLException {
+     // Note: Change the connection parameters accordingly.
+     String hostName = "localhost";
+     String dbName = "uhcaps";
+     String userName = "root";
+     String password = "1234";
+     return getMySQLConnection(hostName, dbName, userName, password);
+ }
+  
+ public static Connection getMySQLConnection(String hostName, String dbName,
+         String userName, String password) throws SQLException,
+         ClassNotFoundException {
+    
+     Class.forName("com.mysql.jdbc.Driver");
+  
+     // URL Connection for MySQL:
+     // Example: 
+     // jdbc:mysql://localhost:3306/simplehr
+     String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+  
+     Connection conn = DriverManager.getConnection(connectionURL, userName,
+             password);
+     return conn;
+ }
+}
